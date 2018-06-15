14b446b27f654b0e33b02c751eb5b8a3a5decf10
David Whitehurst <david@ciwise.com>
2018-06-15 16:30:21 -0400

    added languages

--- src/bip39c.h
+++ src/bip39c.h
@@ -1,38 +1,47 @@
-/* bip39c.h
+/*
+ * The MIT License (MIT)
+ *
+ * Copyright (c) 2018 David L. Whitehurst
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ *
+ * bip39c.h
  * A BIP-39 Implementation using C.
  *
- * Generation of mnemonic words to be used to create
- * a seed for the creation of private keys to be used
- * with the implementation of a deterministic (seeded)
- * bitcoin wallet.
- *
- * written by: David L. Whitehurst
+ * author: David L. Whitehurst
  * date: May 30, 2018
+ *
+ * Find this code useful? Please donate:
+ *  Bitcoin: 1Mxt427mTF3XGf8BiJ8HjkhbiSVvJbkDFY
  *
  */
 
 #ifndef BIP39C_BIP39C_H
 #define BIP39C_BIP39C_H
 
-/*
- * Includes
- */
-#include <unistd.h>
-#include <getopt.h>
-#include <stdio.h>
-#include <stdlib.h>
-#include <stdbool.h>
-#include <limits.h>
-#include <string.h>
-#include <ctype.h>
-#include <openssl/rand.h>
-#include <openssl/err.h>
-#include <openssl/sha.h>
-#include <openssl/evp.h>
+
 
 /*
  * Defines
  */
+
+# define LANG_WORD_CNT   2048
 
 #define BYTE_TO_BINARY_PATTERN "%c%c%c%c%c%c%c%c"
 #define BYTE_TO_BINARY(byte)  \
@@ -79,32 +88,16 @@
   (byte & 0x04 ? '1' : '0'), \
   (byte & 0x02 ? '1' : '0')
 
+/*
+ * Function declarations
+ */
 
-/* function to generate mnemonic sentence */
-int generate(int entropysize);
+void get_mnemonic(int entropysize);
 
-/* function to check values in an array */
-bool isvalueinarray(int val, int *arr, int size);
+void produce_mnemonic_sentence(int segSize, int checksumBits, char *firstByte, char entropy[]);
 
-/* function implementing BIP-39 algorithm */
-int getMnemonic(int entBytes, int csAdd);
+void get_root_seed(const char* pass);
 
-/* function that prints byte array as hex string */
-void printUCharArray(unsigned char bytes[], int size);
-
-/* function that implements SHA-256 using openssl */
-int sha256(char *string, char outputBuffer[65]);
-
-/* function to convert hex string to series of unsigned charactees at pointer */
-unsigned char* hexstr_to_char(const char* hexstr);
-
-/* function to print word found at linenumber */
-void printWord(long lineNumber);
-
-/* function to print entire mnemonic sentence from segmeent (entropy+checksum bits) */
-int produceMnemonicSentence(int segSize, int checksumBits, char *firstByte, char entropy[]);
-
-/* function to print 64 byte (512-bit) derived key from mnemonic */
-void pbkdf2_hmac_sha_512(const char* pass);
+void get_words();
 
 #endif //BIP39C_BIP39C_H
